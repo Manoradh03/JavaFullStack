97f576fb8e5f33f8997e46fd0396ad76ae0dbbc2
Manoradh <mrmad200625@gmail.com>
2026-04-20 19:15:49 +0530

    commit

---
+++ Java/src/Day6/ProductSort.java
@@ -0,0 +1,17 @@
+package Day6;
+
+import java.util.Arrays;
+
+public class ProductSort {
+    public static void main(String[] args) {
+
+        String[] products = {"Laptop", "Mobile", "Charger", "Headphones", "Keyboard"};
+
+        Arrays.sort(products);
+
+        System.out.println("Sorted Product Names:");
+        for (String p : products) {
+            System.out.println(p);
+        }
+    }
+}
